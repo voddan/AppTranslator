e54ce895c6806dea9cc37536972bd29d98efb20a
dvodopian <dgvodopyan@gmail.com>
2023-07-25 19:53:25 +0300

    default project with AppTranslatorService

---
+++ app/src/main/java/io/raviv/apptranslator/AppTranslatorService.kt
@@ -0,0 +1,14 @@
+package io.raviv.apptranslator
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class AppTranslatorService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInterrupt() {
+        TODO("Not yet implemented")
+    }
+}
